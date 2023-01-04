d8c7c53503a9c0da5303ded2d06250c3aaf2838f
Francisco Garcia <jjjchico1@gmail.com>
2023-01-04 15:10:53 -0600

    importation hombe page

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import './App.css'
-
 // components
 import Cursor from './components/cursor'
 
+// pages
+import HombePage from './pages/home' 
 function App() {
 
   return (
     <div className="App">
+      <HombePage />
       
     <Cursor />
     </div>
