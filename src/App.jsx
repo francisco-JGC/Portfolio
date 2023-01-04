35245dd4c9f069b32fd1e4c0f436d5a4999bfd85
Francisco Garcia <jjjchico1@gmail.com>
2023-01-04 12:55:48 -0600

    implenet cursor component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import './App.css'
 
+// components
+import Cursor from './components/cursor'
+
 function App() {
 
   return (
     <div className="App">
       
+    <Cursor />
     </div>
   )
 }
