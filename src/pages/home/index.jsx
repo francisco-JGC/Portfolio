7a7112331dab5746eebe1f49d34af3cb48081e65
Francisco Garcia <jjjchico1@gmail.com>
2023-01-04 15:12:21 -0600

    page home created

---
+++ src/pages/home/index.jsx
@@ -0,0 +1,11 @@
+import DefaultLayout from "../../components/defaultLayout"
+
+export default function HombePage () {
+  return (
+    <DefaultLayout>
+        <div className="home-page">
+            <h1>Home Page</h1>
+        </div>
+    </DefaultLayout>
+  )
+}
