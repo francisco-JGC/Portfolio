a6c2887c721809357f014eee87825866a49cb0f3
Francisco Garcia <jjjchico1@gmail.com>
2023-01-04 15:57:55 -0600

    import header component to default layout

--- src/components/defaultLayout/index.jsx
+++ src/components/defaultLayout/index.jsx
@@ -1,9 +1,11 @@
 import './index.scss'
 import BackgroundLayout from '../backgroundLayout'
+import Header from '../header'
 
 export default function DefaultLayout ({ children }) {
     return (
         <BackgroundLayout>
+            <Header />
             <div className="default-layout">
                 {children}
             </div>
