a2e293b3540ee5a8144763891381a859772f1e97
Francisco Garcia <jjjchico1@gmail.com>
2023-01-22 16:42:15 -0600

    change a sendGrid for emailjs

--- src/services/sendMessageEmail.js
+++ src/services/sendMessageEmail.js
@@ -1,38 +1,27 @@
-import sgMail from '@sendgrid/mail';
+import { init, sendForm } from "@emailjs/browser"
 
 export const sendMessageEmail = async (fields) => {
+    // TODO: emailjs.init() should be called only once in the app
+    init(import.meta.env.VITE_EMAILJS_PUBLIC_KEY)
 
-    sgMail.setApiKey(import.meta.env.VITE_SENDGRID_API_KEY);
-    const cors = 'https://cors-anywhere.herokuapp.com/'
+    const TEMPLATE_FORM = (fields) => `
+            <form>
+                <input type="text" name="from_name" value="${fields.name}" />
+                <input type="email" name="from_email" value="${fields.email}" />
+                <textarea name="message">${fields.message}</textarea>
+            </form>
+        `
+    const template = new DOMParser().parseFromString(TEMPLATE_FORM(fields), "text/html")
+    const form = template.querySelector("form")
 
-    const message = {
-        to: import.meta.env.VITE_SENDGRID_TO_EMAIL,
-        from: fields.email,
-        subject: 'Message from ' + fields.name,
-        text: fields.message,
-        html: `
-                <h1>Message from ${fields.name}</h1>
-                <p>${fields.message}</p>
-            `
-    }
+    /*
+    is necessary create a template and convert to a form
+    because emailjs.sendForm() doesn't work with a simple object as parameter (fields)
+    */
 
-    const sendEmail = async () => {
-        return fetch(cors + 'https://api.sendgrid.com/api/mail.send.json', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-                'Authorization': `Bearer ${import.meta.env.VITE_SENDGRID_API_KEY}`
-            },
-            body: JSON.stringify(message)
-        })
-    }
-
-    try {
-        await sendEmail();
-        return true;
-    } catch (error) {
-        console.log(error);
-        return false;
-    }
-
+    return await sendForm(
+        import.meta.env.VITE_EMAILJS_SERVICE_ID,
+        import.meta.env.VITE_EMAILJS_TEMPLATE_ID,
+        form
+    )
 }
